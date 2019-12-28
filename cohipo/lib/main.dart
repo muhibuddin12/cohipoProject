110168fd314a6d9f390082e0d63ff02f06e58976
Muhibuddin <30959389+muhibuddin12@users.noreply.github.com>
2019-12-28 12:13:03 +0700

    create project

---
+++ cohipo/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:cohipo/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LoginScreen(),
+    );
+  }
+}
---
+++ cohipo/lib/screens/login_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Center(child: Text("Ini halaman login"),)
+        ],
+      ),
+    );
+  }
+}
